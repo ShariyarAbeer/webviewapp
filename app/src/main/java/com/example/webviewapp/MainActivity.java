ca4876d84725a6b7fc5d4fef822420066b9d1e0e
ShariyarAbeer <007abeershariyar@gmail.com>
2021-02-22 07:36:23 +0600

    webview working

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.webviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mywebView=(WebView) findViewById(R.id.webview);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("https://www.fishbox.com.bd/");
+        WebSettings webSettings=mywebView.getSettings();
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+
+
+
+
+
     }
+    @Override
+    public void onBackPressed(){
+        if(mywebView.canGoBack()) {
+            mywebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+
+        }
+    }
+
+   // private class MyBrowser extends WebViewClient {
+    //    @Override
+      //  public boolean shouldOverrideUrlLoading(WebView view, String url) {
+       //     view.loadUrl(url);
+        //    return true;
+      //  }
+    //}
 }
